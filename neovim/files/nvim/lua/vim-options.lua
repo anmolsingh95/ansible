4baa1ba900d5f3c69890560c08300ddbfa940588
Anmol Singh <5581466+anmolsingh95@users.noreply.github.com>
2024-03-22 11:50:57 -0700

    set smartindent vim

--- neovim/files/nvim/lua/vim-options.lua
+++ neovim/files/nvim/lua/vim-options.lua
@@ -1,6 +1,11 @@
-vim.cmd("filetype plugin indent on")
-vim.cmd("set tabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.cmd("set autoindent")
+vim.cmd("set smartindent")
 vim.cmd("set expandtab")
+vim.cmd("filetype plugin indent on")
+
+vim.cmd("autocmd FileType typescript setlocal tabstop=2 shiftwidth=2 expandtab")
+vim.cmd("autocmd FileType javascript setlocal tabstop=2 shiftwidth=2 expandtab")
+vim.cmd("autocmd FileType lua setlocal tabstop=4 shiftwidth=4 expandtab")
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
