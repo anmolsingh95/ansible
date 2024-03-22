de134acd094369fa4c8bdd20b61907d873c13da9
Anmol Singh <5581466+anmolsingh95@users.noreply.github.com>
2024-03-22 12:43:57 -0700

    set more ergonomic neotree settings and keybinds

--- neovim/files/nvim/lua/plugins/neo-tree.lua
+++ neovim/files/nvim/lua/plugins/neo-tree.lua
@@ -7,8 +7,8 @@ return {
         "MunifTanjim/nui.nvim",
     },
     config = function()
-        vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
         require("neo-tree").setup({
+            close_if_last_window=true,
             filesystem = {
                 filtered_items = {
                     visible = true, -- when true, they will just be displayed differently than normal items
@@ -16,7 +16,12 @@ return {
                     hide_gitignored = false,
                     hide_hidden = false, -- only works on Windows for hidden files/directories
                 },
-            }
+                follow_current_file = {
+                    enabled=true,
+                },
+                use_libuv_file_watcher=true,
+            },
         })
+        vim.keymap.set("n", "\\", ":Neotree filesystem reveal left<CR>", {})
     end,
 }
