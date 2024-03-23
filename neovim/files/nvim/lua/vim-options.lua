7b249252120abe622e44a7c6870dfdfe0237be8b
Anmol Singh <5581466+anmolsingh95@users.noreply.github.com>
2024-03-22 20:35:51 -0700

    setup plugin for detecting indentation

--- neovim/files/nvim/lua/vim-options.lua
+++ neovim/files/nvim/lua/vim-options.lua
@@ -2,15 +2,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- set indentation
-vim.cmd 'set autoindent'
-vim.cmd 'set smartindent'
-vim.cmd 'set expandtab'
-vim.cmd 'filetype plugin indent on'
-vim.cmd 'autocmd FileType typescript setlocal tabstop=2 shiftwidth=2 expandtab'
-vim.cmd 'autocmd FileType javascript setlocal tabstop=2 shiftwidth=2 expandtab'
-vim.cmd 'autocmd FileType lua setlocal tabstop=2 shiftwidth=2 expandtab'
-
 -- line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
