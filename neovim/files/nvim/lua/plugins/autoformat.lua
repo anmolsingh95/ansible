1c98dd0868f82198e5c37f3a8534780238966ac7
Anmol Singh <5581466+anmolsingh95@users.noreply.github.com>
2024-03-22 18:50:18 -0700

    change order of python formatters

--- neovim/files/nvim/lua/plugins/autoformat.lua
+++ neovim/files/nvim/lua/plugins/autoformat.lua
@@ -16,7 +16,7 @@ return {
       lua = { 'stylua' },
 
       -- Conform can also run multiple formatters sequentially
-      python = { 'isort', 'black' },
+      python = { 'ruff', 'black', 'isort' },
 
       -- You can use a sub-list to tell conform to run *until* a formatter is found.
       javascript = { { 'prettierd', 'prettier' } },
