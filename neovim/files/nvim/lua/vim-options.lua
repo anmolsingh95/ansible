55a63eec81150b2f65bb94a6369018b3276e0233
Anmol Singh <5581466+anmolsingh95@users.noreply.github.com>
2024-03-22 18:44:28 -0700

    setup stylua autoformatter and format all existing files

--- neovim/files/nvim/lua/vim-options.lua
+++ neovim/files/nvim/lua/vim-options.lua
@@ -3,13 +3,13 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- set indentation
-vim.cmd("set autoindent")
-vim.cmd("set smartindent")
-vim.cmd("set expandtab")
-vim.cmd("filetype plugin indent on")
-vim.cmd("autocmd FileType typescript setlocal tabstop=2 shiftwidth=2 expandtab")
-vim.cmd("autocmd FileType javascript setlocal tabstop=2 shiftwidth=2 expandtab")
-vim.cmd("autocmd FileType lua setlocal tabstop=4 shiftwidth=4 expandtab")
+vim.cmd 'set autoindent'
+vim.cmd 'set smartindent'
+vim.cmd 'set expandtab'
+vim.cmd 'filetype plugin indent on'
+vim.cmd 'autocmd FileType typescript setlocal tabstop=2 shiftwidth=2 expandtab'
+vim.cmd 'autocmd FileType javascript setlocal tabstop=2 shiftwidth=2 expandtab'
+vim.cmd 'autocmd FileType lua setlocal tabstop=4 shiftwidth=4 expandtab'
 
 -- line numbers
 vim.opt.number = true
