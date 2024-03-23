27ebc45de503318a547c96cfb4346b54b2ac3a0b
Anmol Singh <5581466+anmolsingh95@users.noreply.github.com>
2024-03-22 18:20:40 -0700

    setup some basic config

--- neovim/files/nvim/lua/vim-options.lua
+++ neovim/files/nvim/lua/vim-options.lua
@@ -10,3 +10,43 @@ vim.cmd("filetype plugin indent on")
 vim.cmd("autocmd FileType typescript setlocal tabstop=2 shiftwidth=2 expandtab")
 vim.cmd("autocmd FileType javascript setlocal tabstop=2 shiftwidth=2 expandtab")
 vim.cmd("autocmd FileType lua setlocal tabstop=4 shiftwidth=4 expandtab")
+
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
