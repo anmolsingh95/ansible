65b78bb30fe42042b5ce0759f93844299a16cb7d
Anmol Singh <5581466+anmolsingh95@users.noreply.github.com>
2024-04-25 10:56:01 -0700

    add wezterm installation role for ubuntu

--- roles/base/files/.wezterm.lua
+++ roles/base/files/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -8,54 +8,54 @@ local config = wezterm.config_builder()
 
 config.enable_tab_bar = false
 config.automatically_reload_config = true
-config.color_scheme = 'Tokyo Night Storm (Gogh)'
-config.font = wezterm.font('JetBrainsMono Nerd Font Mono', { weight = 'Regular' })
-config.font_size = 14
+config.color_scheme = "Tokyo Night Storm (Gogh)"
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Regular" })
+config.font_size = 12
 config.enable_scroll_bar = true
 config.window_padding = {
-  left = 5,
-  right = 10,
-  top = 12,
-  bottom = 7,
+	left = 5,
+	right = 10,
+	top = 12,
+	bottom = 7,
 }
 
 config.audible_bell = "Disabled"
 
 config.hyperlink_rules = {
-  -- Matches: a URL in parens: (URL)
-  {
-    regex = '\\((\\w+://\\S+)\\)',
-    format = '$1',
-    highlight = 1,
-  },
-  -- Matches: a URL in brackets: [URL]
-  {
-    regex = '\\[(\\w+://\\S+)\\]',
-    format = '$1',
-    highlight = 1,
-  },
-  -- Matches: a URL in curly braces: {URL}
-  {
-    regex = '\\{(\\w+://\\S+)\\}',
-    format = '$1',
-    highlight = 1,
-  },
-  -- Matches: a URL in angle brackets: <URL>
-  {
-    regex = '<(\\w+://\\S+)>',
-    format = '$1',
-    highlight = 1,
-  },
-  -- Then handle URLs not wrapped in brackets
-  {
-    regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
-    format = '$0',
-  },
-  -- implicit mailto link
-  {
-    regex = '\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b',
-    format = 'mailto:$0',
-  },
+	-- Matches: a URL in parens: (URL)
+	{
+		regex = "\\((\\w+://\\S+)\\)",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in brackets: [URL]
+	{
+		regex = "\\[(\\w+://\\S+)\\]",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in curly braces: {URL}
+	{
+		regex = "\\{(\\w+://\\S+)\\}",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in angle brackets: <URL>
+	{
+		regex = "<(\\w+://\\S+)>",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Then handle URLs not wrapped in brackets
+	{
+		regex = "\\b\\w+://\\S+[)/a-zA-Z0-9-]+",
+		format = "$0",
+	},
+	-- implicit mailto link
+	{
+		regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+		format = "mailto:$0",
+	},
 }
 
 -- and finally, return the configuration to wezterm
